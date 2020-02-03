4ea763e59fe964dd99a9bf6dc0745ed0f1029dd6
Anna Lewis Dagnino <annaick1995@gmail.com>
2020-02-03 20:25:27 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -12,7 +12,6 @@ class EmailAddressParser
 
   def parse
     parsed = @email_addresses.split(/[, ]/)
-  #  parsed = parse.uniq
     parsed = parsed.reject{|element| element.empty?}
     parsed.uniq
   end
